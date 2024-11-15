7cb31ad61ba95a797d1f140c3b221cedc06cef5b
Andres Valencia <anvalenciao@poligran.edu.co>
2024-11-14 22:09:43 -0500

    Se resuelve la alerta

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Constants for Gemini API and Model
-const GEMINI_API_KEY = "AIzaSyATrGI8DMuSVEUi8WKxkkPu_501hz8VvKM"; // Replace with your actual API key
+const GEMINI_API_KEY = "YOUR_GEMINI_API_KEY"; // Replace with your actual API key
 const GEMINI_MODEL = "gemini-1.5-flash";
